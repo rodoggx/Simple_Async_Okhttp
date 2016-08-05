87344a822d00f952da43676166ec39d715bef70e
rodoggx <mikerodolph@yahoo.com>
2016-08-05 14:40:49 -0400

    commit 2

--- app/src/main/java/com/example/simpleasyncokhttp/MainActivity.java
+++ app/src/main/java/com/example/simpleasyncokhttp/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -22,18 +24,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doMagic(View view) {
-        String url = "http://www.google.com";
+        String url = "http://www.espn.com";
 
         OkHttpClient client = new OkHttpClient();
         Request request = new  Request.Builder()
                 .url(url)
                 .build();
 
-        try {
+        //Synch call to AsyncTask
+        // client.newCall(request).execute();
+
+        //Async using Okhttp - craete an intenet service n=by itself - triggered automatically
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+        //required with callback
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onTResponse: " + response.body().string());
+            }
+        });
+
+        /*try {
             Response response = client.newCall(request).execute();
             Log.d(TAG, "doMagic: " + response.body().string());
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
